a1f6931bb3d9a612ab68d4853226f8d57b09dde3
Zahra Alzawad <zahra_m.alzawad@hotmail.com>
2021-12-09 20:41:29 +0000

    ..

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tabelView: UITableView!
+    @IBOutlet weak var taskField: UITextField!
+    var tasks: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tabelView.dataSource = self
     }
 
+    @IBAction func beastBtnPressed(_ sender: UIButton) {
+        if let  task = taskField.text {
+            tasks.append(task)
+            tabelView.reloadData()
+        }
+    }
+    
+}
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    
+    
+    
 }
 
